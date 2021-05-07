2a775ee1bccb0515187933722a7f972c9e0e1dbd
Richard Zilahi <zilahi@gmail.com>
2021-05-07 13:56:41 +0300

    Initial commit

---
+++ app/cli/cli.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+/**
+ * CLI
+ * =====================
+ * Command Line Interface
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
+import m from "@app/functions/module";
+import configs from "@configs/config";
+
+(async () => {
+	const { app } = await m({ text: "hello-world" });
+
+	console.log(app());
+	console.log(`debug: ${configs.debug}`);
+})();
